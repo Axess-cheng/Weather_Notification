3bda1a18f3dfd16e9701f3eef7cc5701554001bd
Cheng, Sihan [sgsche20] <sgsche20@pc035030.pctc.liv.ac.uk>
2019-03-26 15:00:10 +0000

    Initial Commit

---
+++ Weather_Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_Reminder
+//
+//  Created by Cheng, Sihan [sgsche20] on 26/03/2019.
+//  Copyright © 2019 comp208.team4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
