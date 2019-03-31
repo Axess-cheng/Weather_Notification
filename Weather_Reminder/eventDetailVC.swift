2de65793dd6bf9494b3d75866d98ec62bcd85147
jiuqiYu <richardnseven@gmail.com>
2019-03-31 14:13:52 +0100

    brief ui

---
+++ Weather_Reminder/eventDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  eventDetailVC.swift
+//  Weather_Reminder
+//
+//  Created by allen on 2019/3/31.
+//  Copyright © 2019 comp208.team4. All rights reserved.
+//
+
+import UIKit
+
+class eventDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
