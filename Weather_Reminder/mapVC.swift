25e9e1d204370711eeef572c44a7ef78333d7311
Axess <axess971230@gmail.com>
2019-04-21 15:34:48 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/UserInterface'"
    This reverts commit c1516bcb5d5525980dd229192a070150071ff549, reversing
    changes made to b7d131058c48a4785515580a13f0025e482ad85b.

--- Weather_Reminder/mapVC.swift
+++ Weather_Reminder/mapVC.swift
@@ -8,103 +8,22 @@
 
 import UIKit
 
-class mapVC: UIViewController,UITableViewDelegate,UITableViewDataSource{
+class mapVC: UIViewController, UITextFieldDelegate {
  @IBOutlet weak var text: UITextField!
-    @IBOutlet weak var tblPlaces: UITableView!
-    var resultsArray:[Dictionary<String, AnyObject>] = Array()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        text.addTarget(self, action: #selector(searchPlaceFromGoogle(_:)), for: .editingChanged)
-        tblPlaces.estimatedRowHeight = 44.0
-        tblPlaces.dataSource = self
-        tblPlaces.delegate = self
+        text.delegate = self
+        // Do any additional setup after loading the view.
     }
     
-    //MARK:- UITableViewDataSource and UItableViewDelegates
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return resultsArray.count
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchPlaceFromGoogelAPI(place: textField.text!)
+        return true
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "placecell")
-        if let lblPlaceName = cell?.contentView.viewWithTag(102) as? UILabel {
-            
-            let place = self.resultsArray[indexPath.row]
-            lblPlaceName.text = "\(place["name"] as! String) \(place["formatted_address"] as! String)"
-        }
-        return cell!
+   
+    func searchPlaceFromGoogelAPI(place: String){
+       var strGoogleAPI = ""
     }
-    
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let place = self.resultsArray[indexPath.row]
-        if let locationGeometry = place["geometry"] as? Dictionary<String, AnyObject> {
-            if let location = locationGeometry["location"] as? Dictionary<String, AnyObject> {
-                if let latitude = location["lat"] as? Double {
-                    if let longitude = location["lng"] as? Double {
-                        UIApplication.shared.open(URL(string: "https://www.google.com/maps/@\(latitude),\(longitude),16z")!, options: [:])
-                    }
-                }
-            }
-        }
-    }
-    
-    
-    
-    @objc func searchPlaceFromGoogle(_ textField:UITextField) {
-        
-        if let searchQuery = textField.text {
-            var strGoogleApi = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(searchQuery)&key= AIzaSyCRI17aiHDgxNUUCz1cnprifJh8rTpZQuw"
-            strGoogleApi = strGoogleApi.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            
-            var urlRequest = URLRequest(url: URL(string: strGoogleApi)!)
-            urlRequest.httpMethod = "GET"
-            let task = URLSession.shared.dataTask(with: urlRequest) { (data, resopnse, error) in
-                if error == nil {
-                    
-                    if let responseData = data {
-                        let jsonDict = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-                        
-                        if let dict = jsonDict as? Dictionary<String, AnyObject>{
-                            
-                            if let results = dict["results"] as? [Dictionary<String, AnyObject>] {
-                                print("json == \(results)")
-                                self.resultsArray.removeAll()
-                                for dct in results {
-                                    self.resultsArray.append(dct)
-                                }
-                                
-                                DispatchQueue.main.async {
-                                    self.tblPlaces.reloadData()
-                                }
-                                
-                            }
-                        }
-                        
-                    }
-                } else {
-                    //we have error connection google api
-                }
-            }
-            task.resume()
-        }
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
     
     /*
     // MARK: - Navigation
